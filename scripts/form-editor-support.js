469c818606fd28b286ae02ce66855de1535679f2
deep <deepprakash345@gmail.com>
2025-12-16 10:46:19 +0530

    chore: doneeee

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -415,4 +415,4 @@ function enableAuthoringAssistantExtension() {
   console.log('Adding meta tag for aem forms authoring assistant extension:', meta.content);
   document.head.appendChild(meta);
 }
-// enableAuthoringAssistantExtension();
+enableAuthoringAssistantExtension();
